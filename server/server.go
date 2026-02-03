d316f66ba6ba2150c3e8692467ed5618912974ba
Jacob Andersen <19645494+jacobsandersen@users.noreply.github.com>
2026-02-03 13:39:35 +0800

    Cleanup server entrypoint

--- server/server.go
+++ server/server.go
@@ -17,9 +17,7 @@ import (
 	"github.com/indieinfra/scribble/server/handler/upload"
 	"github.com/indieinfra/scribble/server/middleware"
 	"github.com/indieinfra/scribble/server/state"
-	"github.com/indieinfra/scribble/storage/content"
 	"github.com/indieinfra/scribble/storage/content/factory"
-	"github.com/indieinfra/scribble/storage/media"
 	mediafactory "github.com/indieinfra/scribble/storage/media/factory"
 	"github.com/indieinfra/scribble/storage/util"
 )
@@ -42,7 +40,6 @@ func StartServer(cfg *config.Config) error {
 		Handler: mux,
 	}
 
-	// Start serving in background to support graceful shutdown.
 	errChan := make(chan error, 1)
 	go func() {
 		log.Printf("serving http requests on %q", srv.Addr)
@@ -51,7 +48,6 @@ func StartServer(cfg *config.Config) error {
 		}
 	}()
 
-	// Listen for termination signals.
 	sigChan := make(chan os.Signal, 1)
 	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
 
@@ -82,25 +78,17 @@ func initialize(st *state.ScribbleState) (*state.ScribbleState, error) {
 	}
 	st.MediaPathPattern = pat
 
-	contentStore, err := initializeContentStore(&st.Cfg.Content)
+	contentStore, err := factory.Create(&st.Cfg.Content)
 	if err != nil {
 		return nil, err
 	}
 	st.ContentStore = contentStore
 
-	mediaStore, err := initializeMediaStore(&st.Cfg.Media)
+	mediaStore, err := mediafactory.Create(&st.Cfg.Media)
 	if err != nil {
 		return nil, err
 	}
 	st.MediaStore = mediaStore
 
 	return st, nil
-}
-
-func initializeContentStore(cfg *config.Content) (content.Store, error) {
-	return factory.Create(cfg)
-}
-
-func initializeMediaStore(cfg *config.Media) (media.Store, error) {
-	return mediafactory.Create(cfg)
 }
